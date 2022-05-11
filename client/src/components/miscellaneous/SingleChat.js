4ccc24105f1507472cc09a9e30ceaca4f20197d3
Tushar <jhamtushar12@gmail.com>
2022-05-12 00:57:54 +0530

    Deployment

--- client/src/components/miscellaneous/SingleChat.js
+++ client/src/components/miscellaneous/SingleChat.js
@@ -19,7 +19,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../../animations/typing.json";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://mern-letschat.herokuapp.com/";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const { userRoutes } = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const { chatRoutes } = require("./routes/chatRoutes");
 const { messageRoutes } = require("./routes/messageRoutes");
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -16,6 +17,19 @@ app.use(userRoutes);
 app.use(chatRoutes);
 app.use(messageRoutes);
 
+// -------------------------deployment-----------------------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running goood");
+  });
+}
+// -------------------------deployment-----------------------------
 app.use(notFound);
 app.use(errorHandler);
 
